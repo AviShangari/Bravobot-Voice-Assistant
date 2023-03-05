703e193aedf9478f876b09785eee37818923d474
AviShangari <shangariaaveg@gmail.com>
2023-03-04 22:10:41 -0500

    Added music functionality and optimized/document code

---
+++ YoutubeMusicPlayer.py
@@ -0,0 +1,102 @@
+from selenium.webdriver.common.by import By
+import undetected_chromedriver as uc
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from dotenv import load_dotenv
+import time
+import os
+
+load_dotenv()
+url = 'https://music.youtube.com/'
+options = uc.ChromeOptions()
+driver = uc.Chrome(options=options, use_subprocess=True)
+driver.get(url)
+USERNAME = os.getenv("YT_USERNAME")
+PASSWORD = os.getenv("YT_PASSWORD")
+
+def log_in():
+    
+
+    WebDriverWait(driver, timeout=10).until(EC.presence_of_element_located((By.CSS_SELECTOR, '.sign-in-link.ytmusic-nav-bar')))
+    driver.find_element(by=By.CSS_SELECTOR, value='.sign-in-link.ytmusic-nav-bar').click()
+
+    username_field = '/html/body/div[1]/div[1]/div[2]/div/c-wiz/div/div[2]/div/div[1]/div/form/span/section/div/div/div[1]/div/div[1]/div/div[1]/input'
+    WebDriverWait(driver, timeout=10).until(EC.presence_of_element_located((By.XPATH, username_field)))
+    driver.find_element(by=By.XPATH, value=username_field).send_keys(USERNAME)
+    driver.find_element(by=By.CSS_SELECTOR, value='.VfPpkd-LgbsSe-OWXEXe-k8QpJ:not(:disabled)').click()
+
+    password_field = '/html/body/div[1]/div[1]/div[2]/div/c-wiz/div/div[2]/div/div[1]/div/form/span/section[2]/div/div/div[1]/div[1]/div/div/div/div/div[1]/div/div[1]/input'
+    WebDriverWait(driver, timeout=10).until(EC.presence_of_element_located((By.XPATH, password_field)))
+    time.sleep(2)
+    driver.find_element(by=By.XPATH, value=password_field).send_keys(PASSWORD)
+    time.sleep(1)
+    driver.find_element(by=By.CSS_SELECTOR, value='.VfPpkd-LgbsSe-OWXEXe-k8QpJ:not(:disabled)').click()
+
+
+def play_song(song: str):
+    search_button = 'tp-yt-paper-icon-button.ytmusic-search-box, input.ytmusic-search-box, input.ytmusic-search-box::placeholder'
+    WebDriverWait(driver, timeout=7).until(EC.presence_of_element_located((By.CSS_SELECTOR, search_button)))
+    driver.find_element(by=By.CSS_SELECTOR, value=search_button).click()
+    driver.find_element(by=By.CSS_SELECTOR, value='input.ytmusic-search-box').send_keys(song)
+    driver.find_element(by=By.CSS_SELECTOR, value='input.ytmusic-search-box').send_keys(Keys.ENTER)
+    time.sleep(2)
+
+    option_lst = '//*[@id="contents"]/ytmusic-responsive-list-item-renderer[1]/div[2]/div[1]/yt-formatted-string/a'
+    WebDriverWait(driver, timeout=5).until(EC.presence_of_element_located((By.XPATH, option_lst)))
+    driver.find_element(by=By.XPATH, value=option_lst).click()
+
+    song_choice = '/html/body/ytmusic-app/ytmusic-app-layout/ytmusic-nav-bar/div[1]/a'
+    WebDriverWait(driver, timeout=5).until(EC.presence_of_element_located((By.XPATH, song_choice)))
+    driver.find_element(by=By.XPATH, value=song_choice).click()
+
+
+def play():
+    play_button = '/html/body/ytmusic-app/ytmusic-app-layout/ytmusic-player-bar/div[1]/div/tp-yt-paper-icon-button[3]'
+    WebDriverWait(driver, timeout=10).until(EC.presence_of_element_located((By.XPATH, play_button)))
+    driver.find_element(by=By.XPATH, value=play_button).click()
+
+
+def pause():
+    pause_button = '/html/body/ytmusic-app/ytmusic-app-layout/ytmusic-player-bar/div[1]/div/tp-yt-paper-icon-button[3]'
+    WebDriverWait(driver, timeout=10).until(EC.presence_of_element_located((By.XPATH, pause_button)))
+    driver.find_element(by=By.XPATH, value=pause_button).click()
+
+
+def repeat(word):
+    if word == 'song':
+        repeat_on = '/html/body/ytmusic-app/ytmusic-app-layout/ytmusic-player-bar/div[3]/div/tp-yt-paper-icon-button[2]'
+        WebDriverWait(driver, timeout=10).until(EC.presence_of_element_located((By.XPATH, repeat_on)))
+        driver.find_element(by=By.XPATH, value=repeat_on).click()
+        time.sleep(0.5)
+        driver.find_element(by=By.XPATH, value=repeat_on).click()
+    if word == 'off':
+        rep_off = '/html/body/ytmusic-app/ytmusic-app-layout/ytmusic-player-bar/div[3]/div/tp-yt-paper-icon-button[2]'
+        WebDriverWait(driver, timeout=10).until(EC.presence_of_element_located((By.XPATH, rep_off)))
+        driver.find_element(by=By.XPATH, value=rep_off).click()
+
+
+def queue(song: str):
+    search_button = 'tp-yt-paper-icon-button.ytmusic-search-box, input.ytmusic-search-box, input.ytmusic-search-box::placeholder'
+    WebDriverWait(driver, timeout=7).until(EC.presence_of_element_located((By.CSS_SELECTOR, search_button)))
+    driver.find_element(by=By.CSS_SELECTOR, value=search_button).click()
+    driver.find_element(by=By.CSS_SELECTOR, value='input.ytmusic-search-box').send_keys(song)
+    driver.find_element(by=By.CSS_SELECTOR, value='input.ytmusic-search-box').send_keys(Keys.ENTER)
+    time.sleep(3)
+
+    sub_lst = '/html/body/ytmusic-app/ytmusic-app-layout/div[3]/ytmusic-search-page/ytmusic-tabbed-search-results-renderer/div[2]/ytmusic-section-list-renderer/div[2]/ytmusic-shelf-renderer[1]/div[3]/ytmusic-responsive-list-item-renderer/ytmusic-menu-renderer/tp-yt-paper-icon-button/tp-yt-iron-icon'
+    WebDriverWait(driver, timeout=5).until(EC.presence_of_element_located((By.XPATH, sub_lst)))
+    driver.find_element(by=By.XPATH, value=sub_lst).click()
+
+    add_to_queue = '/html/body/ytmusic-app/ytmusic-popup-container/tp-yt-iron-dropdown/div/ytmusic-menu-popup-renderer/tp-yt-paper-listbox/ytmusic-menu-service-item-renderer[2]/yt-formatted-string'
+    WebDriverWait(driver, timeout=5).until(EC.presence_of_element_located((By.XPATH, add_to_queue)))
+    driver.find_element(by=By.XPATH, value=add_to_queue).click()
+
+    song_choice = '/html/body/ytmusic-app/ytmusic-app-layout/ytmusic-nav-bar/div[1]/a'
+    WebDriverWait(driver, timeout=5).until(EC.presence_of_element_located((By.XPATH, song_choice)))
+    driver.find_element(by=By.XPATH, value=song_choice).click()
+
+
+def close():
+    driver.quit()
+
--- main.py
+++ main.py
@@ -5,12 +5,18 @@ import tkinter as tk
 import sys
 import os
 import threading
+import time
+import YoutubeMusicPlayer
 from PIL import ImageTk, Image
 from dotenv import load_dotenv
 from neuralintents import GenericAssistant
 
+# Load the env variables and assign them to their respective variables
 load_dotenv()
 openai.api_key = os.getenv("OPEN_API_KEY")
+
+# Log into YouTube Music
+YoutubeMusicPlayer.log_in()
 
 
 class Assistant:
@@ -24,8 +30,11 @@ class Assistant:
         self.speaker.setProperty('voice', voices[0].id)
         self.speaker.setProperty('rate', 200)
 
+        # Set-up the link between the tags in intents.json and their functionality
+        self.intent_mapping = {"ChatGPT": self.ask_anything, "Music": self.play_song, "PausePlayMusic": self.pause_music}
+
         # Initialize and train the Voice Assistant Model with 'Intents'
-        self.assistant = GenericAssistant("intents.json", intent_methods={"ChatGPT": self.ask_anything, "Music": self.play_song})
+        self.assistant = GenericAssistant("intents.json", intent_methods=self.intent_mapping)
         self.assistant.train_model()
 
         # Initialize tkinter and set canvas size
@@ -53,42 +62,61 @@ class Assistant:
         
         self.root.mainloop()
 
+    
+    def get_command(self) -> str:
+        """
+        A function that gets the user command and returns it in the form a string
+        """
+        with sr.Microphone(device_index=1) as mic:
+            self.recognizer.adjust_for_ambient_noise(mic, duration=0.3)
+            audio = self.recognizer.listen(mic)
+
+            
+        text = self.recognizer.recognize_google(audio)
+        return text
+    
+
+    def speak(self, prompt: str) -> None:
+        """
+        A function that speaks the provided prompt. \n
+        Takes a singular string argument in the form: self.speak(prompt: str)
+        """
+        self.speaker.say(prompt)
+        self.speaker.runAndWait()
+
 
     def run_assistant(self) -> None:
+        """
+        Core of the assistant. Waits for user input and then executes commands based on user inputs. \n
+        The user input is executed based on the functionality defined in the 'intents.json' file.
+        """
         while True:
             try:
-                with sr.Microphone(device_index=1) as mic:
-                    self.recognizer.adjust_for_ambient_noise(mic, duration=0.3)
-                    audio = self.recognizer.listen(mic)
+                text = self.get_command()
+                text = text.lower()
 
-                    text = self.recognizer.recognize_google(audio)
+                if "bravo" in text:
+                    self.root.attributes('-alpha', 1)
+                    self.speak("Yes?")
+                    text = self.get_command()
                     text = text.lower()
 
-                    if "bravo" in text:
-                        self.root.attributes('-alpha', 1)
-                        self.speaker.say("Yes?")
-                        self.speaker.runAndWait()
-                        self.recognizer.adjust_for_ambient_noise(mic, duration=0.2)
-                        audio = self.recognizer.listen(mic)
-                        text = self.recognizer.recognize_google(audio)
-                        text = text.lower()
+                    if text == "bye":
+                        self.speak("See you later!")
+                        self.speaker.stop()
+                        self.root.destroy()
+                        YoutubeMusicPlayer.close()
+                        sys.exit()
 
-                        if text == "bye":
-                            self.speaker.say("See you later!")
-                            self.speaker.runAndWait()
-                            self.speaker.stop()
-                            self.root.destroy()
-                            sys.exit()
-
-                        else:
-                            if text is not None:
-                                response = self.assistant.request(text)
-                                
-                                if response is not None:
-                                    self.speaker.say(response)
-                                    self.speaker.runAndWait()
+                    else:
+                        if text is not None:
+                            response = self.assistant.request(text)
                             
-                            self.root.attributes('-alpha', 0.5)
+                            if response is not None:
+                                self.speak(response)
+                        
+                        self.root.attributes('-alpha', 0.5)
+
             except Exception as e:
                 print(e)
                 self.root.attributes('-alpha', 0.5)
@@ -96,23 +124,21 @@ class Assistant:
     
 
     def ask_anything(self) -> None:
+        """
+        Uses OpenAPI to answer any general/specific queries.\n
+        Works by sending a prompt to 'text'davinci-003'engine' and generates a response to return to the user.
+        """
 
         conversation = ""
         username = "Avi"
         bot_name = "BravoBot"
 
-        self.speaker.say("Activating Query Mode. How can I help you today?")
-        self.speaker.runAndWait()
+        self.speak("Activating Query Mode. How can I help you today?")
 
         while True:
             try:
-                self.root.attributes('-alpha', 1)
-                with sr.Microphone(device_index=1) as mic:
-                    self.recognizer.adjust_for_ambient_noise(mic, duration=0.3)
-                    audio = self.recognizer.listen(mic)
-
-                
-                text = self.recognizer.recognize_google(audio)
+                self.root.attributes('-alpha', 1)                
+                text = self.get_command()
                 prompt = username + ":" + text + "\n" + bot_name + ":"
                 
                 conversation += prompt
@@ -132,8 +158,7 @@ class Assistant:
                 conversation += response_str
                 print(response_str)
 
-                self.speaker.say(response_str)
-                self.speaker.runAndWait()
+                self.speak(response_str)
 
                 if text.lower() == 'thank you':
                     self.root.attributes('-alpha', 0.5)
@@ -143,10 +168,29 @@ class Assistant:
                 continue
         
     
-    def play_song(self):
-        
-        self.speaker.say("What song do you want me to play?")
-        self.speaker.runAndWait()
+    def play_song(self) -> None:
+        """
+        Plays a song of the user's preference.\n
+        Functionality includes asking user's song choice and playing it using Selenium.
+        """
+
+        self.speak("What song do you want me to play?")
+        try:
+            self.root.attributes('-alpha', 1)                
+            text = self.get_command()
+            self.speak("Playing " + text)
+            YoutubeMusicPlayer.play_song(text)
+            self.root.attributes('-alpha, 0.5')
+        except:
+            self.root.attributes('-alpha', 0.5)
+    
+    
+    def pause_music(self) -> None:
+        """
+        This function pauses/resumes a song.
+        """
+        YoutubeMusicPlayer.pause()
+
 
 
 if __name__=="__main__":
